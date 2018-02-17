75aa2d0a52d9756c357b21f2edccc1123791c051
Ross Sponholtz <rsponholtz@hotmail.com>
2018-02-17 15:02:27 -0800

    Update testscript.sh

--- CustomScripts/testscript.sh
+++ CustomScripts/testscript.sh
@@ -8,11 +8,13 @@ umount /mnt
 mkfs -t ext4 /dev/sdc1
 mount /dev/sdc1 /mnt
 cd /var
+cat > /root/mntsetup.sh <<EOF
 rsync -aulvXpogtr /var/* /mnt
 cd /
 mv /var /var.old
 mkdir /var
 mount /dev/sdc1 /var
+EOF
 cat > /etc/fstab <<EOF
 /dev/sdc1       /var     ext4    defaults    0 0
 EOF
