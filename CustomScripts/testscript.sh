197e771533be52227b12a403d756b53e361c5b77
Ross Sponholtz <rsponholtz@hotmail.com>
2018-02-17 11:02:19 -0800

    Update testscript.sh

--- CustomScripts/testscript.sh
+++ CustomScripts/testscript.sh
@@ -1,7 +1,19 @@
-# Custom Script for Linux
 # provision new drive
 # attach to /var
 apt-get -y update
 parted -s -a optimal /dev/sdc mklabel msdos
 parted -s -a optimal /dev/sdc mkpart primary ext4 0% 100%
 #parted mounts the drive at /mnt by default
+umount /mnt
+mkfs -t ext4 /dev/sdc1
+mount /dev/sdc1 /mnt
+cd /var
+rsync -aulvXpogtr /var/* /mnt
+cd /
+mv /var /var.old
+mkdir /var
+mount /dev/sdc1 /var
+cat > /etc/fstab <<EOF
+/dev/sdc1       /var     ext4    defaults    0 0
+EOF
+apt-get -y install apache2
